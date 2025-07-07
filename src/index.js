04d30f28ebd4fa3242e7b88d81c837050def2d68
bosslady <estherojile248@gmail.com>
2025-07-07 13:41:34 +0100

    feat: add-component-and-prop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
  import React from "react";
  import ReactDom from "react-dom/client";
+ import "../src/index.css";
+
 
  const pizzaData = [
   {
@@ -49,31 +51,64 @@
 
  function App() {
     return(
-    <div>
+    <div className="container">
     <Header />
     <Menu />
-    {/* <Footer /> */}
+    <Footer />
       </div>
       );
  }
 
- function Header() {
-  return <h1>Fast React Pizza Co.</h1>
- }
-
-function Menu () {
-  return <div>
+ function Menu () {
+  return <main className="menu">
     <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Footer />
-  </div>
+      <Pizza 
+      name="Pizza Spinaci"
+      ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+      photoName="pizzas/spinaci.jpg"
+      price={10}
+      /> 
+
+      <Pizza 
+      name="Pizza Funghi"
+      ingredients="Tomato, mozarella, mushrooms"
+      price={12}
+      photoName="pizzas/funghi.jpg"
+      />
+  </main>
 }
 
+function Pizza(props) {
+  console.log(props);
+   return(
+      <div className="pizza">
+         <img src={props.photoName} alt={props.name}/>;
+         <div>
+         <h3>{props.name} </h3>
+         <p>{props.ingredients}</p>
+         <span>{props.price + 3}</span>
+         </div>
+         
+      </div>
+   )
+}
+
+
+ function Header() {
+
+  // const style = {color:"red", fontSize: "48px", textTransform: "uppercase"}
+  const style = {};
+  return <header className="header">
+    <h1 style={style}
+  >Fast React Pizza Co.
+   </h1>
+  </header>
+ 
+ }
+ 
 function Footer () {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -82,19 +117,9 @@ function Footer () {
   // else alert("Sorry,we're closed");
   
 
-  return <footer>{new Date().toLocaleTimeString()}.We're currently open</footer>
+  return <footer className="footer">{new Date().toLocaleTimeString()}.We're currently open</footer>
 }
 
-function Pizza() {
-   return(
-      <div>
-         <img src='pizzas/spinaci.jpg' alt="Pizza spinaci"/>;
-          <h2>Pizza Spinaci </h2>;
-         <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>;
-
-      </div>
-   )
-}
 
 
 
